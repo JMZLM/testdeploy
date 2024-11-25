16cb5f420ff9179f98443728d4ad1ffe21e216ae
JMZLM <109211163+JMZLM@users.noreply.github.com>
2024-11-25 19:31:20 +0800

    render based app.py

--- app.py
+++ app.py
@@ -26,8 +26,9 @@ MODEL_URL = "https://drive.google.com/uc?id=15T5uc8iMm5Fs8XQIHaRy8W6LYQrVErCQ"
 
 # Function to download YOLO model weights from Google Drive
 def download_model():
-    output_path = "../Yolo-Weights"  # Save to this location
-    gdown.download(MODEL_URL, output_path, quiet=False)
+    output_path = "/opt/render/project/Yolo-Weights"  # Absolute path in Render's environment
+    os.makedirs(output_path, exist_ok=True)  # Ensure the directory exists
+    gdown.download(MODEL_URL, os.path.join(output_path, "best.pt"), quiet=False)
 
 # Modify your YOLO initialization to download the model if not already present
 if not os.path.exists("../Yolo-Weights"):
@@ -35,7 +36,7 @@ if not os.path.exists("../Yolo-Weights"):
     download_model()
 
 # Initialize YOLO model
-model = YOLO("../Yolo-Weights/best.pt")
+model = YOLO("/opt/render/project/Yolo-Weights/best.pt")
 classNames = ["anger", "disgust", "fear", "happy", "neutral", "sad", "neutral"]
 
 # Global variables
